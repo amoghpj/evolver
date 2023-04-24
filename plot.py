1b5ad6cf25c02e932fed948db3902e341f574544
aj@evolver.room <amogh.jalihal@gmail.com>
2023-04-24 18:02:42 -0400

    Fix projection function

--- plot.py
+++ plot.py
@@ -12,6 +12,7 @@ config = yaml.safe_load(f)
 f.close()
 EXP_NAME = config["experiment_settings"]["exp_name"]
 CALIB_NAME = config["experiment_settings"]["calib_name"]
+
 plotthese = {"od_90_raw":{"names":["time","od_90_raw"], "plot": True, "plotvar":"od_90_raw"},
              "od_135_raw":{"names":["time","od_135_raw"], "plot": True, "plotvar":"od_135_raw"},
              "OD":{"names":["time","OD"], "plot": True, "plotvar":"OD"},
@@ -19,6 +20,7 @@ plotthese = {"od_90_raw":{"names":["time","od_90_raw"], "plot": True, "plotvar":
              }
 
 dflist = []
+cdflist = []
 for vial, plotthis in product(list(range(16)), plotthese.keys()):
     _df = pd.read_csv(f"{EXP_NAME}/{plotthis}/vial{vial}_{plotthis}.txt",names=plotthese[plotthis]["names"],
                       skiprows=[0]).astype(float)
@@ -38,14 +40,24 @@ axes = axes.flatten()
 for vial, ax in enumerate(axes):
     rawc90 = pd.read_csv(f"{CALIB_NAME}/od_90_raw/vial{vial}_od_90_raw.txt",
                        skiprows=[0],
-                       names=["time","od90"])
+                       names=["time","90"])
     rawc135 = pd.read_csv(f"{CALIB_NAME}/od_135_raw/vial{vial}_od_135_raw.txt",
                        skiprows=[0],
-                       names=["time","od135"])
-    cdat = calibdf[calibdf.vial == vial]
-    ax.scatter(cdat[90].values, cdat[135].values, label="Calibration Median")
-    ax.scatter(rawc90.od90.values,
-               rawc135.od135.values, c="r", alpha=0.1,label="Calibration Raw Values")    
+                       names=["time","135"])
+    pump = pd.read_csv(f"{CALIB_NAME}/pump_log/vial{vial}_pump_log.txt",
+                       skiprows=[0],
+                       names=["time","pump_duration"])
+    pump["pump_event"] = pump.index.astype(int)
+    cdat = rawc90.merge(rawc135, on="time")
+    cdat["pump_event"] = np.nan
+    for i, prow in pump.iterrows():
+        cdat.loc[(cdat.time <= prow.time) & (np.isnan(cdat.pump_event)), "pump_event"] = prow.pump_event
+    cdat["90_median"] = cdat.groupby("pump_event")["90"].median()
+    cdat["135_median"] = cdat.groupby("pump_event")["135"].median()    
+
+    ax.scatter(cdat["90_median"].values, cdat["135_median"].values, label="Calibration Median")
+    ax.scatter(cdat["90"].values,
+               cdat["135"].values, c="r", alpha=0.1,label="Calibration Raw Values")    
     ddat = df[df.vial == vial]
     ax.scatter(ddat[ddat["datatype"] == "od_90_raw"].od_90_raw,
             ddat[ddat["datatype"] == "od_135_raw"].od_135_raw,
@@ -54,14 +66,14 @@ for vial, ax in enumerate(axes):
                alpha=0.5, label="Chemostat Timecourse")
     ax.set_xlabel("Sensor: 90")
     ax.set_ylabel("Sensor: 135")
-    ax.set_xlim(52000, 63500)
+    #ax.set_xlim(52000, 63500)
 
     ax.set_title(f"Vial {vial}")
     if vial == 15:
         ax.legend()
     
 plt.tight_layout()    
-plt.savefig(f"{EXP_NAME}_projection.pdf")
+plt.savefig(f"{EXP_NAME}_projection.png")
 
 
 plt.close()
