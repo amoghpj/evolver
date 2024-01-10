58120dd4aec3b177203a705c372600e0af3d325d
aj@evolver.room <amogh.jalihal@gmail.com>
2024-01-10 11:43:08 -0500

    Populate setup page with current yaml settings
    Reads the current experiment_parameters.yaml if it exists and
    populates the default values from this file.
    
    Additionally, execute the write_parameters_to_file.py script from the
    browser once the calibration ends. In the future, this script will be
    integrated with the visualize.py script to avoid system calls.

--- visualize.py
+++ visualize.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import shutil
 import time
+from subprocess import Popen, run
 
 def write_config_to_file(config):
     cyaml = yaml.safe_dump(config)
@@ -15,22 +16,57 @@ def write_config_to_file(config):
     with open("./experiment_parameters.yaml","w") as outfile:
         outfile.write(cyaml)
 
+def load_config(path):
+    f = open(path)
+    config = yaml.safe_load(f)
+    f.close()
+    return(config)
+
+OPERATION_MODES = ["calibration",
+                  "growthcurve",
+                  "chemostat",
+                  "turbidostat"]
+
+EVOLVER_NAMES = ["spongebob",
+                 "gary",
+                 "patrick",
+                 "sandy",
+                 "plankton",
+                 "pearl",
+                 "barnacleboy",
+                 "mermaidman",
+                 "squidward",
+                 "krabs"]
+ipdict = {"spongebob":"192.168.1.3",
+          "gary":"192.168.1.6",
+          "patrick":"192.168.1.4",
+          "sandy":"192.168.1.5",
+          "plankton":"192.168.1.14",
+          "pearl": "192.168.1.11",
+          "squidward":"192.168.1.10",
+          "mermaidman":"192.168.1.8",
+          "barnacleboy":"192.168.1.9",              
+          "krabs":"192.168.1.7"}
+
 if __name__ == '__main__':
-    exps = [f for f in os.listdir("./") if (os.path.isdir(f) and f not in  ["__pycache__", "img",".git"])]
+    if os.path.exists("experiment_parameters.yaml"):
+        currentconfig = load_config("experiment_parameters.yaml")
+        expname = currentconfig\
+            .get("experiment_settings", dict())\
+            .get("exp_name", "")
+        exps = [expname]
+    else:
+        currentconfig = None
+        exps = []
+        expname = ""
+
+    exps.extend([f for f in os.listdir("./") if (os.path.isdir(f) and f not in  ["__pycache__", "img",".git",expname])])
     exps.append("Setup")
     exps.append("Current Experiment")    
     page = st.sidebar.selectbox('Experiments..',exps)
     st.title(f"{page}")
-    ipdict = {"spongebob":"192.168.1.3",
-              "gary":"192.168.1.6",
-              "patrick":"192.168.1.4",
-              "sandy":"192.168.1.5",
-              "plankton":"192.168.1.14",
-              "pearl": "192.168.1.11",
-              "squidward":"192.168.1.10",
-              "mermaidman":"192.168.1.8",
-              "barnacleboy":"192.168.1.9",              
-              "krabs":"192.168.1.7"}        
+
+    revipdict = {v:k for k, v in ipdict.items()}
 
     config = {"experiment_settings":
               {"stir_settings":{
@@ -48,30 +84,58 @@ if __name__ == '__main__':
     if page == "Setup":
         with st.form("global_setup"):
             st.header("Global settings")
-            exp_name = st.text_input("Experiment name")
-            ip = st.selectbox("IP address", ["spongebob",
-                                             "gary",
-                                             "patrick",
-                                             "sandy",
-                                             "plankton",
-                                             "pearl",
-                                             "barnacleboy",
-                                             "mermaidman",
-                                             "squidward",
-                                             "krabs"])
+            exp_name = st.text_input("Experiment name",
+                                     value=currentconfig\
+                                     .get("experiment_settings", dict())\
+                                     .get("exp_name", ""))
+            ip = st.selectbox("IP address", EVOLVER_NAMES,
+                              index = EVOLVER_NAMES.index(
+                                  revipdict[currentconfig\
+                                            .get("experiment_settings", dict())\
+                                            .get("ip", "192.168.1.1")]))
 
-            calib = st.text_input("Calibration name")
-            operation = st.selectbox("Operation mode", ["calibration",
-                                                         "growthcurve",
-                                                         "chemostat",
-                                                         "turbidostat"])
-            temperature = st.number_input("Temperature", 25)
-            stir_on_rate = st.number_input("Stir on rate", 0)
-            stir_switch = st.checkbox("Stir rate switch?", False)
-            stir_off_rate = st.number_input("Stir off rate", 0)
-            stir_on_duration = st.number_input("Stir on duration (recommended: 6)", 0)
-            stir_off_duration = st.number_input("Stir off duration (recommended: 6)", 0)
-            estimate_gr = st.checkbox("Estimate Growth Rate?", False)
+            calib = st.text_input("Calibration name",
+                                  value = currentconfig\
+                                     .get("experiment_settings", dict())\
+                                     .get("calib_name", ""))
+            operation = st.selectbox("Operation mode", OPERATION_MODES,
+                                     index = OPERATION_MODES.index(currentconfig\
+                                                                   .get("experiment_settings", dict())\
+                                                                   .get("operation", dict())\
+                                                                   .get("mode","growthcurve")))
+            temperature = st.number_input("Temperature",
+                                          value = currentconfig\
+                                     .get("experiment_settings", dict())\
+                                     .get("temp_all", 25))
+            stir_on_rate = st.number_input("Stir on rate",
+                                           value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("stir_settings", dict())\
+                                           .get("stir_on_rate", 8))
+            stir_switch = st.checkbox("Stir rate switch?",
+                                      value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("stir_settings", dict())\
+                                           .get("stir_switch", False))
+            stir_off_rate = st.number_input("Stir off rate",
+                                            value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("stir_settings", dict())\
+                                           .get("stir_off_rate", 0))
+            stir_on_duration = st.number_input("Stir on duration (recommended: 6)",
+                                               value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("stir_settings", dict())\
+                                           .get("stir_on_duration", 6))
+            stir_off_duration = st.number_input("Stir off duration (recommended: 6)", 
+                                                value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("stir_settings", dict())\
+                                           .get("stir_off_duration", 0))
+            estimate_gr = st.checkbox("Estimate Growth Rate?",
+                                      value = currentconfig\
+                                           .get("experiment_settings", dict())\
+                                           .get("estimate_gr", False))
             st.form_submit_button()
         config["experiment_settings"]["ip"] = ipdict[ip]
         config["experiment_settings"]["exp_name"] = str(exp_name)
@@ -86,7 +150,9 @@ if __name__ == '__main__':
         config["experiment_settings"]["stir_settings"]["stir_off_duration"] = stir_off_duration
         
         config["experiment_settings"]["estimate_gr"] = estimate_gr
-
+        ##############################
+        ## Global settings for Turbidostat and Calibration
+        ##############################
         with st.form("global_details"):
             st.header("Mode specific settings")
             with st.expander("Calibration settings"):
@@ -104,17 +170,24 @@ if __name__ == '__main__':
             
         # if operation == "chemostat":
         #     config["experiment_settings"]["operation"]["chemo_rate"] = chemoset
-            
-        df = pd.DataFrame(
-            [
-                {"Vial": f"{vial}", "to_run": True, "volume":20., 
-                 "calib_initial_od":0.,"calib_end_od":0.,
-                 "turbidostat_low":0.,"turbidostat_high":0.,
-                 "chemo_start_od":0.0, "chemo_start_time":0.0,
-                 "chemo_rate":0.0}
-                for vial in range(16)
-            ]
-        )
+
+        if currentconfig is not None:
+            viallist = []
+            for pvc in currentconfig.get("experiment_settings", dict())\
+                .get("per_vial_settings", []):
+                viallist.append(pvc)
+            df = pd.DataFrame(viallist)
+        else:
+            df = pd.DataFrame(
+                [
+                    {"vial": f"{vial}", "to_run": True, "volume":20., 
+                     "calib_initial_od":0.,"calib_end_od":0.,
+                     "turbidostat_low":0.,"turbidostat_high":0.,
+                     "chemo_start_od":0.0, "chemo_start_time":0.0,
+                     "chemo_rate":0.0}
+                    for vial in range(16)
+                ]
+            )
 
         editeddf = st.experimental_data_editor(df, use_container_width=True)
         for i, row in editeddf.iterrows():
@@ -126,7 +199,7 @@ if __name__ == '__main__':
             else:
                 calib_end_od = row.calib_end_od                
             config["experiment_settings"]["per_vial_settings"].append(
-                {"vial":int(row.Vial),
+                {"vial":int(row.vial),
                  "to_run":row.to_run,
                  "volume":row.volume,
                  "calib_initial_od":row.calib_initial_od,
@@ -141,6 +214,12 @@ if __name__ == '__main__':
         write_config = st.button("Write configuration to file")
         if write_config:
             write_config_to_file(config)
+            
+        if operation == "calibration":
+            st.markdown("If the calibration is complete and the calibration end ODs have been updated, write the calibration to file.")
+            write_calibration = st.button("Write calibration to file")
+            run(["python", "write_calibration_to_file.py"])
+
     elif page == "Current Experiment":
         if os.path.exists("experiment_parameters.yaml"):
             f = open("experiment_parameters.yaml","r")
